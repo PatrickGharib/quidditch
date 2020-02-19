9c0b45fbb0c6a38b81f7786645422ecc7cdc3dd0
PatrickGharib <patrick.a.gharib@gmail.com>
2020-02-18 20:16:22 -0700

    created materials for snitch, walls, and riders

---
+++ Assets/snitchMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snitchMove : MonoBehaviour
+{
+    public GameObject snitch;
+
+    public float force;
+   
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        //return the local neighbourhood of this rider 
+
+        //go through each rider within vicinity and react accordingly attract to similar objects repel others
+
+            force = Random.Range(0, 300);
+           
+            BoidBehaviour(-1);
+            //yield return new WaitForSecondsRealtime(1);
+           
+
+        
+    }
+    //mak objects chas the snitch 
+    public void BoidBehaviour( int forceType)
+    {
+
+        Rigidbody rb = snitch.GetComponent<Rigidbody>();
+        Vector3 direction = transform.position - rb.position;
+        float distance = direction.magnitude;
+        direction = direction.normalized;
+        float forceRate = (force / distance);
+        var temp = direction * (forceRate / rb.mass) * forceType;
+        rb.AddForce(temp);
+    }
+}
