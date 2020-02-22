7b9484cea983277bca8ee3c6772e94008375f218
PatrickGharib <patrick.a.gharib@gmail.com>
2020-02-21 17:22:38 -0700

    started over and got cohesion of boids working also to get off pushing to master

---
+++ Assets/Scripts/Cohesion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MeshCollider))]
+[RequireComponent(typeof(Flyer))]
+public class Cohesion : MonoBehaviour
+{
+    private Flyer flyer;
+    public float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flyer = GetComponent<Flyer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //get local neighbourhood
+        Collider[] neighbours = Physics.OverlapSphere(transform.position, radius);
+        //percieved local middle position of flyer
+        Vector3 midpointAverage = Vector3.zero;
+        int numNeighbours = neighbours.Length;
+
+        foreach(var neighbour in neighbours)
+        {
+            var distance =  neighbour.transform.position - transform.position ;
+            midpointAverage += distance;
+        }
+
+        Debug.Log(flyer + ","+ numNeighbours);
+        if (numNeighbours > 0)
+        {
+            midpointAverage = midpointAverage / numNeighbours;
+            //Lerp is (a-b)*t where t is interpolant, t is bounded [0,1]
+            flyer.velocity += Vector3.Lerp(Vector3.zero, midpointAverage, midpointAverage.magnitude / radius); 
+        }
+       
+    }
+}
---
+++ Assets/Scripts/Flyer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flyer : MonoBehaviour
+{
+    public Vector3 velocity;
+    public float maxVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //limit movement speed to  prevent infinite acceleration
+        if (velocity.magnitude > maxVelocity)
+        {
+            velocity = velocity.normalized * maxVelocity;
+        }
+        //move the rider smoothly
+        this.transform.position += velocity * Time.fixedDeltaTime;
+        //look at the direction of travel
+        transform.rotation = Quaternion.LookRotation(velocity);
+    }
+}
