208ef78fe3bb06cce07e8e510938d81ce0f7931f
PatrickGharib <patrick.a.gharib@gmail.com>
2020-03-01 22:39:49 -0700

    rider respawns into spawn points on either side of the play field based on rider team

---
+++ Assets/Scripts/RespawnRider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnRider : MonoBehaviour
+{
+  
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Equals("Floor")){
+
+            
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
+            if (tag.Equals("Slytherin"))
+            {
+                transform.position = new Vector3(0, 1, (float)22.5);
+                
+            }
+            if(tag.Equals("Gryffindor")) 
+            {
+                transform.position = new Vector3(0, 1, -(float)22.5);
+                 
+            }
+        }
+    }
+
+}
